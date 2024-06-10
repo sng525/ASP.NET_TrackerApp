63444ebbf026a2604e6f0ba6ce0301e156f2003f
Jiaojiao Song <ssssjulia525@gmail.com>
2024-06-10 19:00:08 +0200

    packages & models & db

---
+++ TrackerApp/TrackerApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrackerApp.Models
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
+
